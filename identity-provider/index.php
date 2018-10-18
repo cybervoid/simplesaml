2e3f6927fcbcfe579a47d616fe5807cfaca2c393
Rafael Gil <rafaelgil83@gmail.com>
2018-10-18 11:59:22 -0400

    -small fix to login system

--- identity-provider/index.php
+++ identity-provider/index.php
@@ -5,13 +5,12 @@ if(isset($_POST['login']))
 {
 $status='1';
 $email=$_POST['username'];
-$password=md5($_POST['password']);
-$sql ="SELECT email,password FROM users WHERE email=:email and password=:password and status=(:status)";
+$password=$_POST['password'];
+$sql ="SELECT email,password FROM users WHERE email=:email and AES_DECRYPT(password, 'zhwr2Ie8+AeJSKB2KKIya4KIO3FaYaxKo8MCUFSTcW0=')=:password";
 $query= $dbh -> prepare($sql);
 $query-> bindParam(':email', $email, PDO::PARAM_STR);
 $query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> bindParam(':status', $status, PDO::PARAM_STR);
-$query-> execute();
+
 $results=$query->fetchAll(PDO::FETCH_OBJ);
 if($query->rowCount() > 0)
 {
