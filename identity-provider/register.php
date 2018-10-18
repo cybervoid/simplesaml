9e845024a6893e4ee8b775dd2c51f702b8904126
Rafael Gil <rafaelgil83@gmail.com>
2018-10-18 11:04:48 -0400

    - change password storing method for the idp

--- identity-provider/register.php
+++ identity-provider/register.php
@@ -31,7 +31,7 @@ $querynoti-> bindParam(':notireciver',$reciver, PDO::PARAM_STR);
 $querynoti-> bindParam(':notitype', $notitype, PDO::PARAM_STR);
 $querynoti->execute();    
     
-$sql ="INSERT INTO users(name,email, password, gender, mobile, designation, image, status) VALUES(:name, :email, :password, :gender, :mobileno, :designation, :image, 1)";
+$sql ="INSERT INTO users(name,email, password, gender, mobile, designation, image, status) VALUES(:name, :email, AES_ENCRYPT(:password, 'zhwr2Ie8+AeJSKB2KKIya4KIO3FaYaxKo8MCUFSTcW0='), :gender, :mobileno, :designation, :image, 1)";
 $query= $dbh -> prepare($sql);
 $query-> bindParam(':name', $name, PDO::PARAM_STR);
 $query-> bindParam(':email', $email, PDO::PARAM_STR);
